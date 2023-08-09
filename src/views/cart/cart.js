18d85f472a814b29edb624eb1628cd35ce133028
enyllaoliveira <enyllaoliveira@gmail.com>
2023-08-09 12:30:51 -0300

    layout's cart done

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,7 +1,55 @@
 export default (products) => {
-    console.log(products)
     const cart = document.createElement('div')
-    cart.innerHTML = '<h1> cart </h1>'
+    cart.classList.add('list-itens')
+
+    cart.innerHTML = '<h1> bag </h1>'
+    for(let i = 0; i < products.length; i++) {
+        cart.appendChild(mountItens(products[i].product))
+    }
 
     return cart
 }
+
+function mountItens(product) {
+    
+
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let img = document.createElement('img')
+    img.src = product.imageObjects[0].medium
+    img.classList.add('image')
+
+    let text = document.createElement('span')
+    text.innerText = product.name
+    text.classList.add('tittle')
+
+    let pricies = document.createElement('span')
+    
+    let oldPrice = document.createElement('span')
+    oldPrice.classList.add('oldPrice')
+
+    let price = document.createElement('span')
+    price.innerText =  `R$${product.priceSpecification.originalPrice}` ;
+    price.classList.add('price')
+
+    if (product.priceSpecification.maxPrice !== product.priceSpecification.originalPrice) {
+        oldPrice.innerText = `R$${product.priceSpecification.maxPrice}`
+    } 
+    else {
+        oldPrice.innerText = '';
+    }
+
+    pricies.appendChild(oldPrice)
+    pricies.appendChild(price)
+    card.appendChild(img)
+    card.appendChild(text)
+    card.appendChild(pricies)
+    
+    return card
+    } 
+
+ 
+
+
+
